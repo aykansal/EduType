36a458d894c5833e794c7342dd17f56516ed6904
Ayush <ayushkansal303@gmail.com>
2024-12-26 12:52:38 +0530

    merged thridweb wallet

--- components/ThirdWebWallet.tsx
+++ components/ThirdWebWallet.tsx
@@ -1,22 +1,15 @@
-//@ts-nocheck
 "use client"
 import { createThirdwebClient } from "thirdweb";
 import { ConnectButton } from "thirdweb/react";
-import { getWalletInfo } from "thirdweb/wallets";
-import React, { useEffect, useState } from 'react';
 import { defineChain } from "thirdweb";
 
-
 const chain = defineChain(656476)
-
 const client = createThirdwebClient({
-  clientId: "4f4d7aad88cd12953957137f0f7c0081",
-  chains: [chain], // Ensure the custom chain is recognized by the client
+    clientId: "4f4d7aad88cd12953957137f0f7c0081",
+    // chains: [chain], // Ensure the custom chain is recognized by the client
 });
 
-
 // const [wallets, setWallets] = useState(null);
-
 // useEffect(() => {
 //   const fetchWalletInfo = async () => {
 //     const walletData = await getWalletInfo('io.metamask')
@@ -24,12 +17,6 @@ const client = createThirdwebClient({
 //   }
 //   fetchWalletInfo();
 //   }, []);
-
-
-//   console.log(wallets);
-
-
-
 
 export default function Wallet() {
   return (
